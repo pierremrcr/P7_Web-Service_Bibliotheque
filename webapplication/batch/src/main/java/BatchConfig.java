3d0e0d57ee4de3ee351819cc14903c15fcf722ce
pierremrcr <pierremercier4@gmail.com>
2020-03-31 11:47:53 +0200

    ajout classes module batch

---
+++ webapplication/batch/src/main/java/BatchConfig.java
@@ -0,0 +1,43 @@
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+@ComponentScan
+public class BatchConfig {
+
+    @Autowired
+    public JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    public StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    private MailItemProcessor mailItemProcessor;
+
+
+    @Bean
+    protected Step processorMail() {
+        return stepBuilderFactory
+                .get("mailItemProcessor")
+                .tasklet(mailItemProcessor)
+                .build();
+    }
+
+    @Bean
+    public Job mailJob() {
+        return jobBuilderFactory
+                .get("mailJob")
+                .incrementer(new RunIdIncrementer())
+                .start(processorMail())
+                .build();
+    }
+}
